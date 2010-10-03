d98aebac1617f3878800dac4b4cbcd6052fab1cc
Daniele Ricci <daniele.athome@gmail.com>
2010-10-03 17:07:51 +0200

    Some events :)
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- test_mainwin.c
+++ test_mainwin.c
@@ -3,6 +3,11 @@
 
 static Evas_Object* win = NULL;
 
+static void _gem_clicked(void *data, Evas_Object* obj, const char* emission, const char* source)
+{
+    g_debug("Gem clicked! emission=%s, source=%s", emission, source);
+}
+
 void test_mainwindow()
 {
     win = elm_win_add(NULL, "mokojeweled", ELM_WIN_BASIC);
@@ -13,6 +18,16 @@ void test_mainwindow()
     evas_object_size_hint_align_set(bg, EVAS_HINT_FILL, EVAS_HINT_FILL);
     elm_win_resize_object_add(win, bg);
     evas_object_show(bg);
+
+    Evas_Object* layout = elm_layout_add(win);
+    evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set(layout, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_layout_file_set(layout, "test.edj", "main");
+    elm_win_resize_object_add(win, layout);
+    evas_object_show(layout);
+
+    Evas_Object* edj = elm_layout_edje_get(layout);
+    edje_object_signal_callback_add(edj, "mouse,clicked,1", "table:gem*", _gem_clicked, NULL);
 
     evas_object_resize(win, 400, 400);
     evas_object_show(win);
